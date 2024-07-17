98b77a19553c51cd0daea93d602bc2fef2a3fa27
Osnitz <148634027+Osnitz@users.noreply.github.com>
2024-07-17 17:05:35 +0200

    Delete root_dir/techniques/cleanir directory

--- root_dir/techniques/cleanir/cleanir/tools/named_logs.py
+++
@@ -1,7 +0,0 @@
-def named_logs(model, *logs):
-    result = {}
-    logs = [item for item in logs]
-    for l in zip(model.metrics_names, [logs]):
-        result[l[0]] = l[1]
-    result = result['loss'][0]
-    return result
