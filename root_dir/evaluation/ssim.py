fb34784b33527535c09e3c8eca8a3e3df0bf5c29
geralm <estebanlm@estudiantec.cr>
2024-09-19 22:13:10 +0200

    fixing lpips cuda and cpu issues

--- root_dir/evaluation/ssim.py
+++ root_dir/evaluation/ssim.py
@@ -4,14 +4,8 @@ import lpips
 import torch
 import numpy as np
 from PIL import Image
-
-
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
-
-def resize(img0, img1):
-    # Resize img0 to match img1's size
-    reference_size = img1.size
-    return img0.resize(reference_size)
+from utils import compute_mean_std, get_output_filename,resize_if_different
 # Now you can import the functions and classes
 
 # X: (N,3,H,W) a batch of non-negative RGB images (0~255)
@@ -22,14 +16,13 @@ def main():
     parser.add_argument('path', type=str, nargs=2,
                         help=('Paths of the aligned and deidentified datasets'))
     args = parser.parse_args()
+    #get the only two params
     aligned_dataset_path = args.path[0]
     deidentified_path = args.path[1]
+    #build the ouput files
+    ssim_output_scores_file = get_output_filename("ssim", aligned_dataset_path, deidentified_path)
+    msssim_output_scores_file = get_output_filename("mssim", aligned_dataset_path, deidentified_path)
 
-    dataset_name = aligned_dataset_path.split("/")[-1]
-    technique_name = deidentified_path.split("/")[-2]
-
-    ssim_output_scores_file = f"./root_dir/evaluation/output/ssim_{dataset_name}_{technique_name}.txt" #TODO: fix this to absolute path
-    msssim_output_scores_file = f"./root_dir/evaluation/output/msssim_{dataset_name}_{technique_name}.txt"
     use_gpu = True if torch.cuda.is_available() else False
 
     from torch import nn
@@ -49,28 +42,18 @@ def main():
             # Load images
             aligned_img_path = os.path.join(aligned_dataset_path, file)
             deidentified_img_path = os.path.join(deidentified_path, file)
-            img0 = Image.open(aligned_img_path)
             img1 = Image.open(deidentified_img_path)
-
-            if img0.size != img1.size:
-                img0 = resize(img0, img1)
-
+            img0 = resize_if_different(Image.open(aligned_img_path), img1)
+            #convert to tensor
             img0 = lpips.im2tensor(np.array(img0))  # RGB image from [-1,1]
             img1 = lpips.im2tensor(np.array(img1))  # RGB image from [-1,1]
-             #if img1.shape[2] == 128: # CIAGAN images need to be resized
-            #	#from torchvision import transforms
-            #	#trans = transforms.Compose([transforms.Resize(1024, 1024)])
-            #	img1 = lpips.upsample(img1, (1024, 1024))
 
-            if use_gpu:
-                img0 = img0.cuda()
-                img1 = img1.cuda()
-            else: 
-                img0 = img0.cpu()
-                img1 = img1.cpu()
+            img0.cuda() if use_gpu else img0.cpu()
+            img1.cuda() if use_gpu else img1.cpu()
+
             img0 = (img0 + 1.) / 2.  # [-1, 1] => [0, 1]
             img1 = (img1 + 1.) / 2. 
-            ssim_val = ssim( img0, img1, data_range=1, size_average=True) # return scalar
+            ssim_val = ssim(img0, img1, data_range=1, size_average=True) # return scalar
             ms_ssim_val = ms_ssim( img0, img1, data_range=1, size_average=True ) # return scalar
 
 
@@ -81,10 +64,10 @@ def main():
 
     f.close()
     f_ms.close()
-    ssim_arr = np.loadtxt(ssim_output_scores_file)
-    mssim_arr= np.loadtxt(msssim_output_scores_file)
-    print("ssim | mean & std: {:1.2f}".format(ssim_arr.mean()) + " ± "+"{:1.2f}".format(ssim_arr.std()))
-    print("msssim | mean & std: {:1.2f}".format(mssim_arr.mean()) + " ± "+"{:1.2f}".format(mssim_arr.std()))
+    ssim_mean, ssim_std = compute_mean_std(ssim_output_scores_file)
+    mssim_mean, mssim_std = compute_mean_std(msssim_output_scores_file)
+    print("ssim | mean & std: {:1.2f}".format(ssim_mean) + " ± "+"{:1.2f}".format(ssim_std))
+    print("msssim | mean & std: {:1.2f}".format(mssim_mean) + " ± "+"{:1.2f}".format(mssim_std))
     
 
 
